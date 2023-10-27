85b661bc258f60a86bf64533590f91532cc4768b
mike <lixiaofeng1115@outlook.com>
2023-10-27 18:46:17 +0800

    142: 环形链表II

---
+++ src/main/java/org/example/leetcode/editor/cn/_142_环形链表II.java
@@ -0,0 +1,110 @@
+package org.example.leetcode.editor.cn;
+//给定一个链表的头节点 head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到
+//链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。 
+//
+// 不允许修改 链表。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [3,2,0,-4], pos = 1
+//输出：返回索引为 1 的链表节点
+//解释：链表中有一个环，其尾部连接到第二个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2], pos = 0
+//输出：返回索引为 0 的链表节点
+//解释：链表中有一个环，其尾部连接到第一个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：head = [1], pos = -1
+//输出：返回 null
+//解释：链表中没有环。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围在范围 [0, 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵ 
+// pos 的值为 -1 或者链表中的一个有效索引 
+// 
+//
+// 
+//
+// 进阶：你是否可以使用 O(1) 空间解决此题？ 
+//
+// Related Topics 哈希表 链表 双指针 👍 2373 👎 0
+
+
+import java.util.HashSet;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        //return this.useHashSet(head);
+        return this.twoPointers(head);
+    }
+
+    ListNode useHashSet(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+
+        while (head != null) {
+            if (!set.add(head)) {
+                return head;
+            }
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    ListNode twoPointers(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode temp = head;
+
+        while (fast != null) {
+            fast = fast.next;
+            if (fast == null) {
+                break;
+            }
+            fast = fast.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                while (temp != slow) {
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                fast = temp;
+                break;
+            }
+        }
+
+        return fast;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
